81b7fceab3265ef4b1474a4f57a5b2484ac100dc
Yegor Kolesnikov <kelegorkel@gmail.com>
2015-02-03 19:02:34 +0200

    added add function

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ var data = [];
         return elem.id !== guest.id;
       });
     };
+
+    this.add = function (newName) {
+      var newId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+      });
+      var guest = {
+        id: newId,
+        name: newName,
+        inHall: false
+      };
+      this.guests.push(guest);
+    };
   }]);
 }());
 
