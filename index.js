30a78f9255ff0ec94b6aaaa334cc7b495697ddbc
Yegor Kolesnikov <kelegorkel@gmail.com>
2015-02-03 19:25:20 +0200

    saving results on server

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var ws = new WebSocket("ws://f2.smartjs.academy/ws");
-var data = [];
-
 (function() {
   var app = angular.module("hw1App", []);
 
@@ -12,14 +9,37 @@ var data = [];
         ctrl.guests = data;
       });
 
+    var ws = new WebSocket("ws://f2.smartjs.academy/ws");
+    ws.onmessage = function(evt){
+      var addGuest = {
+        id: JSON.parse(evt.data).guest.id,
+        name: JSON.parse(evt.data).guest.name,
+        inHall: JSON.parse(evt.data).guest.inHall
+      };
+
+      var removeGuest = JSON.parse(evt.data).remove.id;
+      console.log(removeGuest);
+      ctrl.guests.push(addGuest);
+    };
+
     this.move = function (guest) {
       guest.inHall = !guest.inHall;
+
+      ws.send(JSON.stringify({
+        action: 'update',
+        guest: guest
+      }));
     };
 
     this.del = function (guest) {
       this.guests = this.guests.filter(function (elem) {
         return elem.id !== guest.id;
       });
+
+      ws.send(JSON.stringify({
+        action: 'remove',
+        id: guest.id
+      }));
     };
 
     this.add = function (newName) {
@@ -27,69 +47,19 @@ var data = [];
         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
         return v.toString(16);
       });
+
       var guest = {
         id: newId,
         name: newName,
         inHall: false
       };
+
       this.guests.push(guest);
+
+      ws.send(JSON.stringify({
+        action: 'add',
+        guest: guest
+      }));
     };
   }]);
 }());
-
-//$(function () {
-//    .on('click', '.item .delete', function () {
-//      var id = $(this).parents('.item').data('id');
-//      data = _.reject(data, { id: id });
-//
-//      ws.send(JSON.stringify({
-//        action: 'remove',
-//        id: id
-//      }));
-//
-//      render();
-//      return false;
-//  });
-//
-//  $('#addGuest').on('click', function(){
-//    var id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-//      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-//      return v.toString(16);
-//    });
-//
-//    var valName = $('#name').val();
-//
-//    var guest = {
-//      id: id,
-//      name: valName,
-//      inHall: false
-//    };
-//
-//    data.push(guest);
-//    ws.send(JSON.stringify({
-//      action: 'add',
-//      guest: guest
-//    }));
-//    render();
-//    return false;
-//  });
-//
-//});
-
-ws.onmessage = function(evt){
-  var addGuest = {
-    id: JSON.parse(evt.data).guest.id,
-    name: JSON.parse(evt.data).guest.name,
-    inHall: JSON.parse(evt.data).guest.inHall
-  };
-
-  var removeGuest = JSON.parse(evt.data).remove.id;
-  console.log(removeGuest);
-  data.push(addGuest);
-
-  //render();
-};
-
-
-
-
